23125bb7ba6671311b71d61289c95ea41b711ad8
Marco Godínez <markotom@gmail.com>
2015-06-29 00:08:40 -0500

    The basic example was enhanced

--- examples/basic.js
+++ examples/basic.js
@@ -5,8 +5,8 @@ var joi = require('joi');
 var validator = require('../index');
 var server = restify.createServer();
 
-server.use(restify.bodyParser());
-server.use(restify.queryParser());
+server.use(restify.bodyParser({ mapParams: false }));
+server.use(restify.queryParser({ mapParams: false }));
 server.use(validator());
 
 server.post({
@@ -19,13 +19,25 @@ server.post({
       param1: joi.string().required(),
       param2: joi.string().email(),
       param3: joi.string().uri()
-    },
-    query: {
-      param4: joi.string()
     }
   }
 }, function (req, res) {
-  console.log(req.params, req.body, req.query);
+  console.log(req.params, req.body);
+  res.send(201);
+});
+
+server.get({
+  path: '/article/:id',
+  validations: {
+    params: {
+      id: joi.string().regex(/^[0-9a-fA-F]{24}$/)
+    },
+    query: {
+      param1: joi.string().required()
+    }
+  }
+}, function (req, res) {
+  console.log(req.params, req.query);
   res.send(200);
 });
 
